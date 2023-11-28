bcb7874bce3bc126d1b0723e166df8c90200fd8c
hs06146 <0_030_@naver.com>
2023-11-28 21:00:25 +0900

    feat: validation

---
+++ class/section10/10-04-typeorm-crud-validation/src/apis/products/entities/product.entity.ts
@@ -0,0 +1,58 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { ProductCategory } from 'src/apis/productsCategories/entities/productCategory.entity';
+import { ProductSalesLocation } from 'src/apis/productsSalesLocations/entities/productSalesLocation.entity';
+import { ProductTag } from 'src/apis/productsTags/entities/productTag.entity';
+import { User } from 'src/apis/users/entites/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  name: string;
+
+  @Column()
+  @Field(() => String)
+  description: string;
+
+  @Column()
+  @Field(() => Int)
+  price: number;
+
+  @Column({ default: false })
+  @Field(() => Boolean)
+  isSoldout: boolean;
+
+  @JoinColumn() // One To One(1:1) 관계에서는 중심이 되는 Entity에 JoinColumn 데코레이션을 추가해주어야 함.
+  @OneToOne(() => ProductSalesLocation) // 1:1 연결인데 ProductSalesLocation 테이블이랑 연결할거야. OneToOne : Product To ProductSalesLocation
+  @Field(() => ProductSalesLocation)
+  productSalesLocation: ProductSalesLocation;
+
+  @ManyToOne(() => ProductCategory) // Many: Product, One: ProductCategory
+  @Field(() => ProductCategory)
+  productCategory: ProductCategory;
+
+  @ManyToOne(() => User) // Many: Product, One: User
+  @Field(() => User)
+  user: User;
+
+  // ManyToMany 는 연결되는 두 테이블 모두에 추가해주어야 함.
+  @JoinTable()
+  @ManyToMany(() => ProductTag, (productTags) => productTags.products) // 첫번째 인자는 어느 테이블과 연결되는지, 두번째 인자는 상대 테이블이 나를 뭐라 지칭하는지
+  @Field(() => [ProductTag])
+  productTags: ProductTag[];
+}
