1b2d9105ce0df440b23631179a20de708dff2f51
hs06146 <0_030_@naver.com>
2023-11-30 01:49:26 +0900

    feat: CRUD 1:1

---
+++ class/section10/10-06-typeorm-crud-one-to-one/src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { BoardsModule } from './apis/boards/boards.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './apis/products/products.module';
+
+@Module({
+  imports: [
+    BoardsModule, //
+    ProductsModule,
+    // UsersModule,
+    ConfigModule.forRoot({
+      // envFilePath: './.env',
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: './src/commons/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DATABASE_TYPE as 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE, // 이건 미리 만들어주어야함.
+      entities: [__dirname + '/apis/**/*.entity.*'], // typescript는 이후 js로 바뀌기 때문에 확장자를 js로 하거나 *로 한다.
+      logging: true,
+      synchronize: true,
+    }),
+  ],
+})
+export class AppModule {}
