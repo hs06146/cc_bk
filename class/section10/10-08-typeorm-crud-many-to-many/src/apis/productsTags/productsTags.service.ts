1d145a082ece0499e152556bdb35569e73242dc7
hs06146 <0_030_@naver.com>
2023-11-30 07:17:05 +0900

    feat: CRUD many-to-many, homework: fix productService-update

---
+++ class/section10/10-08-typeorm-crud-many-to-many/src/apis/productsTags/productsTags.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { In, Repository } from 'typeorm';
+import { ProductTag } from './entities/productTag.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+  IProductTagsServiceFindByNames,
+  IProductsTagsServiceBulkInsert,
+} from './interfaces/products-tags-service.interface';
+
+@Injectable()
+export class ProductsTagsService {
+  constructor(
+    @InjectRepository(ProductTag)
+    private readonly productsTagsRepository: Repository<ProductTag>,
+  ) {}
+
+  findByNames({ tagNames }: IProductTagsServiceFindByNames) {
+    return this.productsTagsRepository.find({
+      where: { name: In(tagNames) },
+    });
+  }
+
+  bulkInsert({ names }: IProductsTagsServiceBulkInsert) {
+    return this.productsTagsRepository.insert(names); // bulk-insert(배열이 들어갔을 때 배열에 들어간 내용이 한 번에 저장됨)
+  }
+}
