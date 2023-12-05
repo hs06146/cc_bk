0532f558df34e726f806da42fea370e690b04ebd
hs06146 <0_030_@naver.com>
2023-12-05 10:59:13 +0900

    feat: RefreshToken, set-Cookie

---
+++ class/section11/11-04-login-refresh-cookie/src/apis/users/users.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { UsersResolver } from './users.resolver';
+import { UsersService } from './users.service';
+import { User } from './entites/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      User, //
+    ]),
+  ],
+  providers: [
+    UsersResolver, //
+    UsersService,
+  ],
+  exports: [
+    UsersService, //
+  ],
+})
+export class UsersModule {}
