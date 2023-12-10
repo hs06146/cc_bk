51757354a0809c18af8c043f72462118315bcb2f
hs06146 <0_030_@naver.com>
2023-12-10 22:11:01 +0900

    feat: Transaction - Iamport

---
+++ class/section12/12-01-point-transaction/backend/src/apis/pointsTransactions/entities/pointTransaction.entity.ts
@@ -0,0 +1,49 @@
+import { Field, Int, ObjectType, registerEnumType } from '@nestjs/graphql';
+import { User } from 'src/apis/users/entites/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+export enum POINT_TRANSACTION_STATUS_ENUM {
+  PAYMENT = 'PAYMENT',
+  CANCEL = 'CANCEL',
+}
+
+registerEnumType(POINT_TRANSACTION_STATUS_ENUM, {
+  name: 'POINT_TRANSACTION_STATUS_ENUM',
+});
+
+@Entity()
+@ObjectType()
+export class PointTransaction {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  impUid: string;
+
+  @Column()
+  @Field(() => Int)
+  amount: number;
+
+  @Column({ type: 'enum', enum: POINT_TRANSACTION_STATUS_ENUM })
+  @Field(() => POINT_TRANSACTION_STATUS_ENUM)
+  // status: '결제완료' 또는 '취소완료';
+  status: POINT_TRANSACTION_STATUS_ENUM;
+
+  @ManyToOne(() => User)
+  @Field(() => User)
+  user: User;
+
+  @CreateDateColumn()
+  @Field(() => Date)
+  createdAt: Date;
+
+  // updatedAt: Date; // Insert Only 테이블로, 수정 내용은 추가하지 않음. 같은 impUid 에 amount 를 -, status를 '취소완료' 와 같은 방식으로 추가함.
+}
