8bec16fb7782d07bf4514f8bb828f553b9b8234c
hs06146 <0_030_@naver.com>
2023-12-12 01:40:43 +0900

    feat: Isonation

---
+++ class/section12/12-04-isonation/backend/src/apis/auth/strategies/jwt-refresh.strategy.ts
@@ -0,0 +1,22 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+// import {KakaoStrategy} from 'passport-kakao'
+// import {NaverStrategy} from 'passport-naver';
+// import {GoogleStrategy} from 'passport-google'; 처럼 전략패턴을 사용하여 strategy를 교체한다.
+export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'refresh') {
+  constructor() {
+    super({
+      jwtFromRequest: (req) => {
+        const cookie = req.headers.cookie;
+        const refreshToken = cookie.replace('refreshToken=', '');
+        return refreshToken;
+      },
+      ignoreExpiration: false,
+      secretOrKey: '나의리프레시비밀번호', // 비밀번호
+    });
+  }
+
+  async validate(payload: any) {
+    return { id: payload.sub }; // => req.user = { id: payload.sub }
+  }
+}
