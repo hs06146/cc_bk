d55c6f2dc9008f643604cb9a39f7958a7d291acc
hs06146 <0_030_@naver.com>
2023-12-12 01:58:54 +0900

    feat: Isolation - Serializable

---
+++ class/section12/12-05-point-transaction-isolation/backend/src/apis/pointsTransactions/pointsTransactions.service2.ts
@@ -0,0 +1,84 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import {
+  POINT_TRANSACTION_STATUS_ENUM,
+  PointTransaction,
+} from './entities/pointTransaction.entity';
+import { IPointsTransactionsServiceCreate } from './interfaces/points-transactions-service.interface';
+import { User } from '../users/entites/user.entity';
+
+@Injectable()
+export class PointsTransactionsService {
+  constructor(
+    @InjectRepository(PointTransaction)
+    private readonly pointsTransactionsRepository: Repository<PointTransaction>,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+
+    private readonly dataSource: DataSource,
+  ) {}
+
+  async create({
+    impUid,
+    amount,
+    user: _user,
+  }: IPointsTransactionsServiceCreate): Promise<PointTransaction> {
+    // this.pointsTransactionsRepository.create // 등록을 위한 빈 객체 만들기
+    // this.pointsTransactionsRepository.insert // 결과는 못 받는 등록 방법
+    // this.pointsTransactionsRepository.update // 결과는 못 받는 수정 방법
+
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction('SERIALIZABLE');
+    try {
+      // 1. PointTransaction 테이블에 거래기록 1줄 생성
+      const pointTransaction = this.pointsTransactionsRepository.create({
+        impUid,
+        amount,
+        user: _user,
+        status: POINT_TRANSACTION_STATUS_ENUM.PAYMENT,
+      });
+      // await this.pointsTransactionsRepository.save(pointTransaction);
+      await queryRunner.manager.save(pointTransaction);
+
+      // // 2. 유저의 돈 찾아오기
+      // // const user = await this.usersRepository.findOne({
+      // //   where: { id: _user.id },
+      // // });
+      // const user = await queryRunner.manager.findOne(User, {
+      //   where: { id: _user.id }, // row-lock
+      //   lock: { mode: 'pessimistic_write' }, // 돈이 업데이트 되는 과정(계산 중)에 다른 사람이 조회를 하면 계산이 꼬여버림.
+      // });
+
+      // // 3. 유저의 돈 업데이트
+      // const updatedUser = this.usersRepository.create({
+      //   ...user, // 여기에 id 값이 있으면 update로 동작함.
+      //   point: user.point + amount,
+      // });
+      // await queryRunner.manager.save(updatedUser);
+
+      // 2. 유저의 돈 찾아서 업데이트하기 => 숫자일때만 가능 ==> lock을 걸지 않아도 됨.
+      await queryRunner.manager.increment(
+        User, // User 테이블에서
+        { id: _user.id }, // id가 _user.id 인 row에
+        'point', // point 라는 데이터를
+        amount, // amount 만큼 증가시켜줘.
+      );
+
+      // 저장 내용 반영하기
+      await queryRunner.commitTransaction();
+
+      // 3. 최종 결과 브라우저에 돌려주기
+      return pointTransaction;
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+    } finally {
+      // 연결 끊기
+      queryRunner.release();
+      // Database의 max_connection 갯수는 한정되어있음.
+      // release 를 하지 않으면 commit이 끝나도 커넥션이 안끊겨서 db_connection status가 sleep 상태로 됨. (하지만, 에러나면 강제로 끊김)
+      // 결국 연결 갯수 초과로 그 다음 사람은 무한 대기 상태가 됨.
+    }
+  }
+}
---
+++ class/section12/12-05-point-transaction-isolation/backend/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './commons/filter/http-exception.filter';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // validation 적용
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.enableCors({
+    origin: true, //여기에 url을 넣어도된다.
+    credentials: true,
+  });
+  await app.listen(3000);
+}
+bootstrap();
