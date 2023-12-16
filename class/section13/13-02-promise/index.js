68bde0d337b5bce8d98e58b0a014ea3427285385
hs06146 <0_030_@naver.com>
2023-12-17 02:07:45 +0900

    feat: Promise

---
+++ class/section13/13-02-promise/index.js
@@ -0,0 +1,19 @@
+const fetchData = () => {
+  // API 보내기 요청!!
+
+  const result = new Promise((성공시함수, 실패시함수) => {
+    setTimeout(() => {
+      try {
+        console.log("이미지 받아왔다~"); // 5초 뒤에 이미지 받아옴
+        성공시함수("강아지.jpg");
+      } catch (err) {
+        실패시함수("실패했습니다!!");
+      }
+    }, 5000);
+  });
+  console.log(result);
+
+  console.log("받아온 강아지.jpg 브라우저 전달");
+};
+
+fetchData();
